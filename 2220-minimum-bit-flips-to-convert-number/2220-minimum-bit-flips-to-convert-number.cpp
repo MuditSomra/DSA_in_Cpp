2bd25a5e339e2f18bbf15d98e0ffef1a45806ec2
Mudit Somra <113372073+MuditSomra@users.noreply.github.com>
2026-06-30 16:15:34 +0530

    Time: 0 ms (100%), Space: 8.1 MB (25.91%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        start = start ^ goal;
+        goal = 0;
+        while(start!=0){
+            start = start & ( start - 1);
+            goal ++;
+        }
+        return goal;
+    }
+};
