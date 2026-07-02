3fc77861f2fdc97a4bf189fdca5c38ce777e4793
Mudit Somra <113372073+MuditSomra@users.noreply.github.com>
2026-07-02 09:54:02 +0530

    Time: 12 ms (83.82%), Space: 26.5 MB (26.41%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -7,16 +7,13 @@ public:
             sort(s.begin(), s.end());
             temp.push_back(s);
         }
-        unordered_map<string, vector<int>> mp(strs.size());
+        unordered_map<string, vector<string>> mp(strs.size());
         for(int i = 0; i < temp.size(); i++){
-            mp[temp[i]].push_back(i);
+            mp[temp[i]].push_back(strs[i]);
         }
         for(auto it= mp.begin(); it != mp.end(); it++){
-            vector<string> tstr;
-            for(int x: it->second){
-                tstr.push_back(strs[x]);
-            }
-            ans.push_back(tstr);
+          
+            ans.push_back(it->second);
         }
 
 
