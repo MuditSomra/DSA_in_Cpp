45b5ade93fc3d8846ed141916345f404e1f82834
Mudit Somra <113372073+MuditSomra@users.noreply.github.com>
2026-07-03 13:41:38 +0530

    Time: 0 ms (100%), Space: 10.7 MB (40.83%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    void recur(int ind , vector<int>& nums, vector<int> &ds, vector<vector<int>> &ans, vector<bool> freq ){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
-            return;
-        }
-        for(int i = 0; i < nums.size(); i++){
-        if(!freq[i]){
-            freq[i] = true;
-            ds.push_back(nums[i]);
-            recur(ind+1, nums, ds, ans, freq);
-            ds.pop_back();
-            freq[i]=false;
-        }
 
+    void recur(int ind, vector<int> & nums,  vector<vector<int>> &ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+        }
+        for(int i = ind; i < nums.size(); i++){
+            swap(nums[ind],nums[i]);
+            recur(ind+1, nums, ans);
+            swap(nums[ind], nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> ds;
+         vector<vector<int>> ans;
+        // vector<int> ds;
         // bool * freq = new bool[nums.size()];
-        vector<bool> freq(nums.size(), false);
-        recur(0, nums, ds, ans, freq);
+        // vector<bool> freq(nums.size(), false);
+        recur(0, nums, ans);
         return ans;
     }
 };
