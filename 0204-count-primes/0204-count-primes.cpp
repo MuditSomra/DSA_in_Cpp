bf95d2bdf6f16842a839f1e67d7af26f64bb8bc2
Mudit Somra <113372073+MuditSomra@users.noreply.github.com>
2026-07-07 11:35:31 +0530

    Time: 132 ms (95.6%), Space: 132.6 MB (15.88%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -9,24 +9,29 @@ public:
         arr[0]=0;
         // if(n!=0){
         arr[1]=0;
-        
+        // int count = 0;
         for(int i = 2; i * i <= n; i++){
 
             if(arr[i]==1){
                 // cout<<"Inside if with i "<<i<<endl;
-                for(int j = i+i; j<n; j+=i){
+                // count++;
+                for(int j = i*i; j<n; j+=i){
+                    
                 // cout<<"making multiple of i ="<<i<<" and number j "<<j<<" zero "<<endl;
                     arr[j]=0;
                 }
             }
         }
         int count = 0;
-        for(int i = 0; i < n; i++){
-            if(arr[i]==1){
-                // cout<<i<<" ";
-                count++;
-            }
+        for(int x: arr){
+            count+=x;
         }
+        // for(int i = 0; i < n; i++){
+        //     if(arr[i]==1){
+        //         // cout<<i<<" ";
+        //         count++;
+        //     }
+        // }
         // for(int x: arr){
         //     cout<<x<<" ";
         // }
