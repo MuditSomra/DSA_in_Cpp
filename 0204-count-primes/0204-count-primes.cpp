ca917797772f70539c9c044f76aba5e0f5211b97
Mudit Somra <113372073+MuditSomra@users.noreply.github.com>
2026-07-07 11:29:53 +0530

    Time: 151 ms (88.87%), Space: 132.8 MB (8.92%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<int> arr(n,1);
+        if(n==0 || n==1){
+            return 0;
+        }
+        
+        arr[0]=0;
+        // if(n!=0){
+        arr[1]=0;
+        
+        for(int i = 2; i * i <= n; i++){
+
+            if(arr[i]==1){
+                // cout<<"Inside if with i "<<i<<endl;
+                for(int j = i+i; j<n; j+=i){
+                // cout<<"making multiple of i ="<<i<<" and number j "<<j<<" zero "<<endl;
+                    arr[j]=0;
+                }
+            }
+        }
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i]==1){
+                // cout<<i<<" ";
+                count++;
+            }
+        }
+        // for(int x: arr){
+        //     cout<<x<<" ";
+        // }
+        // cout<<endl;
+        return count;
+    }
+};
