49186d54e6a204f7b75f1e6372ed9567a24744a7
Mudit Somra <113372073+MuditSomra@users.noreply.github.com>
2026-07-14 12:42:01 +0530

    Time: 3 ms (74.03%), Space: 28.7 MB (22.8%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,33 +2,34 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
        stack<int> st;
-       vector<int> temp(nums.size(),-1);
-        for(int i = nums.size()-1; i>=0; i--){
-            while(!st.empty() && nums[i]>=st.top()){
+        int n = nums.size();
+       vector<int> temp(n,-1);
+        for(int i = (2 * n)-1; i>=0; i--){
+            while(!st.empty() && nums[i%n]>=st.top()){
                 st.pop();
             }
             if(st.empty()){
-                temp[i]=-1;
+                temp[i%n]=-1;
             }
             else{
-                temp[i]=st.top();
+                temp[i%n]=st.top();
             }
-            st.push(nums[i]);
+            st.push(nums[i%n]);
         }
-        for(int i = nums.size()-1; i>=0; i--){
-            // while(!st.empty()){
-            while(!st.empty() && nums[i]>=st.top()){
-                st.pop();
-            }
-            if(st.empty()){
-                temp[i]=-1;
-            }
-            else{
-                temp[i]=st.top();
-            }
-            st.push(nums[i]);
-            // }
-        }
+        // for(int i = nums.size()-1; i>=0; i--){
+        //     // while(!st.empty()){
+        //     while(!st.empty() && nums[i]>=st.top()){
+        //         st.pop();
+        //     }
+        //     if(st.empty()){
+        //         temp[i]=-1;
+        //     }
+        //     else{
+        //         temp[i]=st.top();
+        //     }
+        //     st.push(nums[i]);
+        //     // }
+        // }
         
        
         return temp;  
