6aa8f79ebf208fda2226a89697dbf2273d92c831
Mudit Somra <113372073+MuditSomra@users.noreply.github.com>
2026-07-23 10:14:21 +0530

    Time: 0 ms (100%), Space: 18.8 MB (76.46%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        map<int,char> mp;
+        // map<int,char> mp;
+        // for(int i = 0; i < indices.size(); i++){
+        //     mp[indices[i]]=s[i];
+        // }
+        // string ans=""; 
+        // for(auto it: mp){
+        //     ans += it.second;
+        // }
+        // return ans;
+        string ans(indices.size(),' ');
         for(int i = 0; i < indices.size(); i++){
-            mp[indices[i]]=s[i];
-        }
-        string ans=""; 
-        for(auto it: mp){
-            ans += it.second;
+            ans[indices[i]]=s[i];
         }
         return ans;
     }
