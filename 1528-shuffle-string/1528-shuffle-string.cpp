20ade7850549a1024699f072a81ae1711001153c
Mudit Somra <113372073+MuditSomra@users.noreply.github.com>
2026-07-23 10:08:13 +0530

    Time: 6 ms (4.59%), Space: 20.4 MB (5.64%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        map<int,char> mp;
+        for(int i = 0; i < indices.size(); i++){
+            mp[indices[i]]=s[i];
+        }
+        string ans=""; 
+        for(auto it: mp){
+            ans += it.second;
+        }
+        return ans;
+    }
+};
