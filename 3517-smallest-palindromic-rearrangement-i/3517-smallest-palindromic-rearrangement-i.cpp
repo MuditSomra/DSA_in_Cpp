41acfa77d39101d333be530e6f6ef4ffba4349a6
Mudit Somra <113372073+MuditSomra@users.noreply.github.com>
2026-07-28 12:46:18 +0530

    Time: 213 ms (5.05%), Space: 72.5 MB (52.75%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+       map<char,int> mp;
+       bool found = false;
+       char fv;
+       for(char x: s){
+        mp[x]++;
+       } 
+       string ans = "";
+       for(auto &it: mp){
+        if((it.second%2)!=0){
+            found = true;
+            fv = it.first;
+            if(it.second>2){
+                int val = it.second/2;
+                for(int i = 0; i < val; i++){
+                    ans+=it.first;
+                    it.second--;
+                }
+            }
+        }
+        else{
+           int val = it.second/2;
+                for(int i = 0; i < val ; i++){
+                    ans+=it.first;
+                    it.second--;
+                }
+            }
+        
+
+        }
+       
+       if(found){
+        ans += fv;
+        mp[fv]--;
+       }
+       for(auto it = mp.rbegin(); it != mp.rend(); it++){
+        // if(it->second%2!=0){
+        //     if(it->second>2){
+        //         while(it->second!=1){
+        //             ans+=it->first;
+        //             it->second--;
+        //         }
+        //     } 
+        // }
+        // else{
+        while(it->second!=0){
+            ans+=it->first;
+            it->second = it->second - 1;
+        }
+        // }
+        
+        }
+        return ans;
+    }
+};
